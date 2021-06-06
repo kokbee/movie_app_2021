88b82f4368a084f536bb45c9f02926bde1e10f23
김민수 <kokbee@kokbee.local>
2021-06-06 23:34:12 +0900

    create beta branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import Movie from "./Movie";
 import "./App.css";
 
-
 class App extends React.Component {
   state = {
     isLoading: true,
--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-
 function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
