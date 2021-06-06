e987410a71708eabd507b9760a59b1b697894f73
김민수 <kokbee@kokbee.local>
2021-06-06 22:33:41 +0900

    axios test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import PropTypes from "prop-types";
 import React from "react";
+import axios from "axios";
 
-class App extends React.Component{
+class App extends React.Component {
     state = {
-        isLoading :true
+      isLoading: true,
+      movies: []
     };
-
-    componentDidMount(){
-        setTimeout(() => {
-            this.setState({isLoading:false});
-        }, 6000);
+    getMovies = async () => {
+      const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    };
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({ isLoading: false });
+      }, 6000);
+      this.getMovies();
     }
-
+    // 삼항 연산자를 사용해 렌더가 된 후 componentDidMount를 통해 값을 변경하여 출력 
     render () {
         const {isLoading} = this.state;
         return (
